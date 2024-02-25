05d7994ce91684feee9a130dbd9b615fe5f2eb8f
Dan Lourenco <danlourenco@gmail.com>
2024-02-25 12:44:02 -0500

    letter count

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
   num_words = get_num_words(text)
   #print(text)
   print(num_words)
+  letters = get_letter_count(text)
+  print(letters)
 
 def get_book_text(path):
   with open(path) as f:
@@ -12,4 +14,17 @@ def get_book_text(path):
 def get_num_words(text):
   return len(text.split())
 
+def get_letter_count(text):
+  letters = {}
+  words = text.split()
+  for word in words:
+    lowered_word = word.lower()
+    for letter in lowered_word:
+      if letter in letters:
+        letters[letter] += 1
+      else:
+        letters[letter] = 1
+  
+  return letters
+    
 main()
